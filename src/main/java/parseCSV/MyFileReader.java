5bcbbefceca5cf1c64568fdf4269c341ae8da687
thufir <hawat.thufir@gmail.com>
2020-02-16 05:05:47 -0800

    incremental

--- src/main/java/parseCSV/MyFileReader.java
+++ src/main/java/parseCSV/MyFileReader.java
@@ -3,6 +3,9 @@ package parseCSV;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -10,6 +13,7 @@ public class MyFileReader {
 
     private static final Logger log = Logger.getLogger(MyFileReader.class.getName());
     private Properties properties = null;
+    private List<String> lines = null;//new ArrayList<String>();
 
     private MyFileReader() {
     }
@@ -19,11 +23,11 @@ public class MyFileReader {
     }
 
     public void readFile() throws IOException {
-          BufferedReader reader = new BufferedReader(new FileReader(properties.getProperty("dataFile")));       
-          reader.lines().forEach(line -> processLine(line));
+        BufferedReader reader = new BufferedReader(new FileReader(properties.getProperty("dataFile")));
+        lines = new ArrayList();
+        reader.lines().forEach(line -> lines.add(line));
+        Collections.reverse(lines);
+        log.info(lines.toString());
     }
 
-    private void processLine(String line) {
-        log.info(line);
-    }
 }
